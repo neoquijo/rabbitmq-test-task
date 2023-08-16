b79db9a2c969748c03a7e38efb1445fb8edfeb4c
neoquijo <91754365+neoquijo@users.noreply.github.com>
2023-08-16 12:55:34 +0200

    Update rmq.service.ts
    noAck:true

--- shared/RMQ/rmq.service.ts
+++ shared/RMQ/rmq.service.ts
@@ -19,14 +19,4 @@ export class RmqService {
         }
     }
 
-
-
-
-
-
-    ack(context: RmqContext) {
-        const channel = context.getChannelRef();
-        const originalMessage = context.getMessage();
-        channel.ack(originalMessage);
-    }
 }
